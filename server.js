587d3cf4d732ecb278b998af9841e55eb785e334
ElverArevalo <elver.arevalo@gmail.com>
2020-06-22 16:01:05 -0500

    =correcion de url6

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/catalogDos'));
+app.use(express.static(__dirname+'/dist/catalogoDos'));
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/catalogDos/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/catalogoDos/index.html'));
 });
 
 app.listen(process.env.PORT || 8000);
